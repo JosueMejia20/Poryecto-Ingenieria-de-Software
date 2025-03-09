32268e693e98fb9b05800091a1917814bb2405b3
JosueMejia20 <josuedanielm21@gmail.com>
2025-03-08 21:11:36 -0600

    Primera Version Login

---
+++ Vistas/login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/Poryecto-Ingenieria-de-Software/public/css/style_login.css">
+</head>
+<body>
+    <div class="container-form" id="Cuerpo_login">
+        <div class="informacion">
+            <div class="info-childs">
+                <h2>Se Parte de la UNAH</h2>
+                <p>Recuerda que antes debe de completar el proceso
+                    de admision 
+                </p>
+                <a href="incio.html">
+                    <input type="button" value="Regresar">
+                </a>
+            </div>
+        </div>
+        <dive class="form-informacion">
+            <div class="form-informacion-child">
+                <h2>Inicia Sesion</h2>
+                <p>Incia sesion con tu numero de cuenta y contraseña</p>
+                <form class="form">
+                    <label>
+                        <i class="bi bi-person"></i>
+                        <input type="text" placeholder="Nuemero de Cuenta">
+                    </label>
+                    <label>
+                        <i class="bi bi-lock"></i>
+                        <input type="password" placeholder="Contraseña">
+                    </label>
+                    <input type="submit" value="Acceder">
+                </form>
+            </div>
+        </dive>
+    </div>
+    
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js">
+    </script>
+
+</body>
+</html>
