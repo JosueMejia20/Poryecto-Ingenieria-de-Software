fb019ba77b10878501459023e81f8231f3754162
g3rrarrd <g_rodriguez51@outlook.com>
2025-03-09 11:20:26 -0600

    modified:   Controladores/Autenticacion/jwtHelper.php modified:   Controladores/includes/userLogin.class.php

--- Controladores/Autenticacion/jwtHelper.php
+++ Controladores/Autenticacion/jwtHelper.php
@@ -10,7 +10,7 @@
      * @version 1.0.0.
      *
      */
-class helper{
+class Helper{
 
     /** Llama a la funcion create del arhivo createJwt.php para generar un token de autenticacion.
      *
@@ -42,33 +42,31 @@ class helper{
     /** Llama a la funcion validate del arhivo validateJwt.php para verificar la autenticacion del token.
      *
      * @param string $jwt recibe un token jwt.
-     * @param string $secretKey recibe la contrasenia del usuario.
+     * @param string $secretKey recibe una llave secreta
      * @return string retorna un valor booleano segun la validez.
      *
      * @author Gerardo Antonio Rodriguez Contreras .
-     * @version 1.0.0.
+     * @version 1.1.0.
      *
      */
     public static function validate($jwt, $secretKey){
         
-    
         $jwt_validate = new validate();
 
         $payload = $jwt_validate->validate($jwt, $secretKey);
 
-        if ($payload){
-            return True;
-            
-        }else{
-            return False;
+        if($payload){
+            return true;
             
         }
         
+        return false;
+    
     }
 
 }
 
-$helper = new helper();
+$helper = new Helper();
 
 return $helper;
 
--- Controladores/includes/userLogin.class.php
+++ Controladores/includes/userLogin.class.php
@@ -10,22 +10,26 @@
             $json = ['status'=>false, 'jwt'=>null, 'Rol'=>null];
 
             $query = $mysqli->prepare
-            ('SELECT count(*) AS existe, usr.UsuarioID as id, Rol
+            ('SELECT usr.UsuarioID as id, Rol
             FROM Usuario AS usr
             INNER JOIN Estudiante AS std
+            ON usr.UsuarioID = std.UsuarioID
             INNER JOIN Docente AS dct
+            ON usr.UsuarioID = dct.UsuarioID
             INNER JOIN Coordinador AS cdr
+            ON usr.UsuarioID = cdr.UsuarioID
             WHERE (std.CorreoInstitucional = ? AND std.contrasenia = ?)
+            OR (dct.CorreoInstitucional = ? AND dct.contrasenia = ?)
+            OR (cdr.CorreoInstitucional = ? AND cdr.contrasenia = ?)
             GROUP BY usr.UsuarioID, ROL');
 
-            $query->bind_param('ss', $userName, $passWord);
+            $query->bind_param('ssssss', $userName, $passWord,$userName, $passWord,$userName, $passWord);
 
             $query->execute();
             $result = $query->get_result();
             $row = $result->fetch_assoc();
 
-            if ($row['existe'] == 1){
-                
+            if ($result->nums_rows > 1){
                 $json['status'] = true;
                 $json['jwt'] = $jwt->create($row['id'], $userName, $env['SECRET_WORD']);
                 $json['Rol'] = $row['Rol'];
@@ -36,7 +40,14 @@
             return $json;
         }
 
+        public static function validateJwt($idUser){
 
+            $jwt = include "../Autenticacion/jwtHelper.php";
+            $env = include "enviroment.php";
+
+
+            return $jwt->validateJwt($idUser, $env['SECRET_WORD']);
+        }
     }
 
 ?>
